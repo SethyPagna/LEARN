73e81b12c770e1ad3a8a1fdd8d3917429ee03f19
SethyPagna <ungsethypagna30@gmail.com>
2026-05-13 18:50:38 +0800

    deploy: remove pages proxy

--- deploy/cloudflare-pages-proxy/_worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    if (!env.LEARN_WORKER) {
-      return new Response("LEARN Worker service binding is not configured.", { status: 503 })
-    }
-
-    return env.LEARN_WORKER.fetch(request)
-  },
-}
