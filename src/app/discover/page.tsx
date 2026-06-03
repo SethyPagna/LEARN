9de33ad9659909c7fe6ca1337fa1fb0770043550
SethyPagna <ungsethypagna30@gmail.com>
2026-06-03 19:24:19 +0800

    Redirect discover to dashboard

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,5 +1,5 @@
-import { LearnShell } from "@/components/learn/learn-shell"
+import { redirect } from "next/navigation"
 
 export default function DiscoverPage() {
-  return <LearnShell initialView="discover" />
+  redirect("/dashboard")
 }
